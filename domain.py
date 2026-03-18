5fdca7723df519686ffc38a09ab6aa1d4d8db929
i.chach <ivantchatch@yandex.ru>
2026-03-18 20:57:48 +0300

    feat: fix: add const var for service and comments

--- domain.py
+++ domain.py
@@ -3,17 +3,22 @@ from dataclasses import dataclass
 from datetime import date
 from typing import Optional, List
 
+#Доменная модель для представления курса валют, бизнес-логика
 @dataclass
 class CurrencyRate:
     code: str
     value: float
     date: date
-
+    
+    #Интерфейс(абстрактный класс) для репо курсов валют
 class CurrencyRepository(ABC):
+    
+    #Получения курса конкретной валюты
     @abstractmethod
     async def get_rate(self, currency_code: str, rate_date: Optional[date] = None) -> Optional[CurrencyRate]:
-        pass
+        pass  
     
+    #Получения курса всех валют
     @abstractmethod
     async def get_all_rates(self, rate_date: Optional[date] = None) -> List[CurrencyRate]:
         pass
--- infrastructure.py
+++ infrastructure.py
@@ -4,16 +4,19 @@ from datetime import date
 from typing import Optional, List
 from domain import CurrencyRate, CurrencyRepository
 
+#реализация репозитория для получения курсов валют с ЦБ РФ
 class CbrRepository(CurrencyRepository):
     def __init__(self, base_url: str):
         self.base_url = base_url
     
+    #получения курса на конкретную дату, даты нет - возвращаем сегодня
     async def get_rate(self, currency_code: str, rate_date: Optional[date] = None) -> Optional[CurrencyRate]:
         url = self.base_url
         if rate_date:
             date_str = rate_date.strftime("%d/%m/%Y")
             url = f"{self.base_url}?date_req={date_str}"
         
+        #Асинхронный HTTP-запрос
         async with httpx.AsyncClient() as client:
             try:
                 resp = await client.get(url)
@@ -21,14 +24,16 @@ class CbrRepository(CurrencyRepository):
                 
                 if "Error in parameters" in resp.text:
                     return None
-                
+                #Парсинг XML-ответов 
                 root = ET.fromstring(resp.text)
                 
+                #Преобразование ответа от API ЦБ РФ из XML
                 for valute in root.findall('Valute'):
                     if valute.find('CharCode').text == currency_code:
                         value_str = valute.find('Value').text.replace(',', '.')
                         value = float(value_str)
                         
+                        # Возвращаем объект доменной модели с курсом валюты
                         return CurrencyRate(
                             code=currency_code,
                             value=value,
@@ -38,8 +43,10 @@ class CbrRepository(CurrencyRepository):
                 return None
                 
             except:
+                #При ошибках - пустой ответ
                 return None
     
+    #Получение всех валют за конкретную дату
     async def get_all_rates(self, rate_date: Optional[date] = None) -> List[CurrencyRate]:
         url = self.base_url
         if rate_date:
