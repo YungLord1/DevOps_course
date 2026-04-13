c4b639b878814cb91ee9ed41da0af7975df9990b
i.chach <ivantchatch@yandex.ru>
2026-04-13 03:46:32 +0300

    feat: edit ports

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ app.include_router(router)
 if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv('PORT', '8000'))
-    uvicorn.run(app, host="127.0.0.1", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
