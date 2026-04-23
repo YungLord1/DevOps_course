db388489df82bd842b1cfd1353d17fd4b2aea9e2
i.chach <ivantchatch@yandex.ru>
2026-04-24 01:32:04 +0300

    feat: edit ansible

---
+++ ansible/collections/ansible_collections/demo/demo/roles/kube/molecule/default/tests/test_default.py
@@ -0,0 +1,19 @@
+import os
+import testinfra.utils.ansible_runner
+"""Role testing files using testinfra."""
+
+def test_hosts_file(host):
+    """Validate /etc/hosts file."""
+    f = host.file("/etc/hosts")
+
+    assert f.exists
+    assert f.user == "root"
+    assert f.group == "root"
+
+def test_kube_packages_installed(host):
+    for pkg in ["kubelet", "kubeadm", "kubectl"]:
+        assert host.package(pkg).is_installed
+
+def test_kubelet_service_enabled(host):
+    service = host.service("kubelet")
+    assert service.is_enabled
